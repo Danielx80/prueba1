5d6b3dd95e51f01c1d6f72ea73e8b11372f665ab
unknown <danielmondragon777@gmail.com>
2021-01-15 15:52:56 -0600

    actualizacion

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express=require('express');
-const path=require('path');
-const app=express();
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use('/',express.static(path.join(__dirname,'/public')));
 
-app.listen(3000,()=>{
-    console.log("Servidor en el puerto 3000");
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+app.listen(3000, () => {
+    console.log("Servidor en el puerto" + port);
 })
